4eacaea027746eefa248bedd1c1b560d0f775b5c
Kennion Gubler <gublerkennion@gmail.com>
2023-10-10 14:39:51 -0700

    Initial Commit

---
+++ Silent Alarm/Silent_AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Silent_AlarmApp.swift
+//  Silent Alarm
+//
+//  Created by Kennion Gubler on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Silent_AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
